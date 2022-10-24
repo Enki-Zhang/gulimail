63030d3f82f8bf54db98e65166a23e4c129ff86a
Enki-Zhang <17855378518@163.com>
2022-10-24 15:05:29 +0800

    init commit

---
+++ guilimail-order/src/test/java/com/atguigu/gulimail/guilimailorder/GuilimailOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimail.guilimailorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilimailOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ guilimail-ware/src/test/java/com/atguigu/guilimail/guilimailware/GuilimailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.guilimail.guilimailware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilimailWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
